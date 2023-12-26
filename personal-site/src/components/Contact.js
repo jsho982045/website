ed0087b71b0544d5132e61611669831e383db18d
Jack E. Shomer <74559111+jsho982045@users.noreply.github.com>
2023-12-26 04:23:22 +0000

    fixed Contact.js

--- personal-site/src/components/Contact.js
+++ personal-site/src/components/Contact.js
@@ -6,18 +6,18 @@ function Contact() {
   return (
     <section id="contact">
       <h2>Contact Me</h2>
-      <p>Include your email, LinkedIn profile, or other contact information here.</p>
+      <p></p>
       <div className="icon-container">
       {/* Gmail Icon */}
-      <a href="mailto:jackshomer98@gmail.com" target="_blank" className="contact-icon">
+      <a href="mailto:jackshomer98@gmail.com" target="_blank" rel="noreferrer noopener" className="contact-icon">
         <img src="gmailIcon2.png" alt="Gmail" />
       </a>
       {/* LinkedIn Icon */}
-      <a href="https://www.linkedin.com/in/jack-shomer-067123266/" target="_blank" className="contact-icon">
+      <a href="https://www.linkedin.com/in/jack-shomer-067123266/" target="_blank" rel="noreferrer noopener" className="contact-icon">
         <img src="linkedin.png" alt="LinkedIn" />
       </a>
       {/* GitHub Icon */}
-      <a href="https://github.com/jsho982045" target="_blank" className="contact-icon">
+      <a href="https://github.com/jsho982045" target="_blank" rel="noreferrer noopener" className="contact-icon">
         <img src="githubIcon1.svg" alt="GitHub" />
       </a>
     </div>
